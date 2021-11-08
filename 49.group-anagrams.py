35267b61cc39930881f4b38759ad925ac98ddcce
akihirosasaki <akihirosasaki0730@gmail.com>
2021-11-09 00:11:38 +0900

    solved problem 20211108

--- 49.group-anagrams.py
+++ 49.group-anagrams.py
@@ -7,6 +7,11 @@
 # @lc code=start
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
+        d={}
+        for w in sorted(strs):
+            key=tuple(sorted(w))
+            d[key]=d.get(key, []) + [w]
+        return d.values()
+
 # @lc code=end
 
