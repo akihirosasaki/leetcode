e1643e2cbc158ec264a0b2756d64458a667256ed
akihirosasaki <akihirosasaki0730@gmail.com>
2021-11-21 10:22:40 +0900

    while文でMove Zeros accepted

--- 283.move-zeroes.py
+++ 283.move-zeroes.py
@@ -10,11 +10,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)-1):
-            print(i,nums)
-            if nums[i]==0 and nums[i+1]!=0:
-                nums[i]=nums[i+1]
-                nums[i+1]=0
+        slow,fast=0,1
+        while fast<len(nums):
+            if nums[slow]==0 and nums[fast]!=0:
+                nums[slow]=nums[fast]
+                nums[fast]=0
+                slow+=1
+                fast+=1
+            elif nums[slow]==0 and nums[fast]==0:
+                fast+=1
+            else:
+                slow+=1
+                fast+=1
+            
             
                 
 
