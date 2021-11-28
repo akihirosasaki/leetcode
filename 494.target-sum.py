8ac46a6f6b6f28236927d8808c9ba9b306c0664a
akihirosasaki <akihirosasaki0730@gmail.com>
2021-11-28 10:49:00 +0900

    solved problem Target Sum

---
+++ 494.target-sum.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=494 lang=python3
+#
+# [494] Target Sum
+#
+
+# @lc code=start
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return 0
+        memo={}
+        return self.dfs(nums,target,memo)
+
+    def dfs(self, nums, target, memo):
+        if tuple(nums+[target]) in memo:
+            return memo[tuple(nums+[target])]
+        if not nums:
+            if target==0:
+                return 1
+            return 0
+        cnt=0
+        cnt+=self.dfs(nums[1:], target-nums[0], memo)
+        cnt+=self.dfs(nums[1:], target+nums[0], memo)
+        memo[tuple(nums+[target])]=cnt
+        return cnt
+        
+# @lc code=end
+
