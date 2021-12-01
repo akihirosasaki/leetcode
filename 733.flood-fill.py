57051249d07057f25abad08276749539b6c3f9e6
akihirosasaki <akihirosasaki0730@gmail.com>
2021-12-01 21:56:55 +0900

    solved problem Flood Fill

--- 733.flood-fill.py
+++ 733.flood-fill.py
@@ -7,24 +7,29 @@
 # @lc code=start
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        visited=[ [False]*len(image) for i in range(len(image[0]))]
+        visited=[ [False]*len(image[0]) for i in range(len(image))]
         rl=len(image)
         cl=len(image[0])
+        self.output=copy.deepcopy(image)
         self.dfs(image,sr,sc,rl,cl,newColor,image[sr][sc], visited)
-        return image
+        return self.output
+        
 
     def dfs(self, image: List[List[int]], sr: int, sc: int, rl: int, cl: int, newColor: int, baseColor: int, visited: List[List[bool]]):
-        if (sc<0 or sc>=cl or sr<0 or sr>=rl or image[sc][sr]!=baseColor or visited[sr][sc]):
+        print(sr,sc,image[0][2],visited)
+        if (sc<0 or sc>=cl or sr<0 or sr>=rl):
+            return 
+        if image[sr][sc]!=baseColor or visited[sr][sc]:
             return 
 
         visited[sr][sc]=True
         
-        image[sr][sc]=newColor
+        self.output[sr][sc]=newColor
         self.dfs(image, sr+1, sc, rl, cl, newColor, baseColor, visited)
         self.dfs(image, sr-1, sc, rl, cl, newColor, baseColor, visited)
         self.dfs(image, sr, sc+1, rl, cl, newColor, baseColor, visited)
         self.dfs(image, sr, sc-1, rl, cl, newColor, baseColor, visited)
-        
+    
         
 
 # @lc code=end
