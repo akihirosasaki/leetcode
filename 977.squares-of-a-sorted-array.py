bf5cfd37aeebf0de74a7bdf7b84928c71256603d
akihirosasaki <akihirosasaki0730@gmail.com>
2021-12-04 10:42:26 +0900

    solved problem Squares of a Sorted Array

---
+++ 977.squares-of-a-sorted-array.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=977 lang=python3
+#
+# [977] Squares of a Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted([x*x for x in nums])
+        
+
+# @lc code=end
+
