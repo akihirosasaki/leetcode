cf8345aa2cdddefda44a83a8cc4187fee6a3c500
akihirosasaki <akihirosasaki0730@gmail.com>
2021-12-04 13:57:45 +0900

    solved problem Replace Elements with Greatest Element on Right Side

---
+++ 1299.replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1299 lang=python3
+#
+# [1299] Replace Elements with Greatest Element on Right Side
+#
+
+# @lc code=start
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        if len(arr)==1:
+            return [-1]
+
+        for i in range(len(arr)-1):
+            arr[i]=max(arr[i+1:])
+        arr[-1]=-1
+        return arr
+# @lc code=end
+
